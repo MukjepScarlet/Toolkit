2aeba10c252941ab7a6aea51013162960d37d5a6
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2023-01-29 19:00:55 +0800

    添加日志系统和多账号支持

--- Python/贴吧自动签到.py
+++ Python/贴吧自动签到.py
@@ -1,13 +1,14 @@
 # 使用方法：
-# 找到cookie_text变量，在Chrome或Edge等浏览器中进入贴吧首页并登录，按F12调出开发者界面，进入网络/Network选项，刷新网页，找到最上方的贴吧首页GET请求，复制请求的COOKIE文本，放入即可。
+# 在脚本目录下新建文件config.json，内容为一个字符串数组，里面放上需要使用的COOKIES，可以放多个，COOKIE文本中有双引号，请用转义符号
+# 在Chrome或Edge等浏览器中进入贴吧首页并登录，按F12调出开发者界面，进入网络/Network选项，刷新网页，找到最上方的贴吧首页GET请求，复制请求的COOKIE文本，放入即可。
 # 自动运行方法：使用Windows的计划任务功能，打开控制面板搜索进入，新建任务，选择触发器(TRIGGER)，每天在你使用电脑中途运行一次即可。(例如每天下午六点整，随机延迟5分钟)
-# "程序或脚本"填入你的Python路径，参数设置成脚本路径，工作目录可以不填。(此脚本暂未追加日志功能)
+# "程序或脚本"填入你的Python路径，参数设置成脚本文件的路径，工作目录填写脚本所在的目录
 # NETA自：https://github.com/sakura-flutter/tampermonkey-scripts
-# 功能：直接运行APP端签到，开调试模式查看tasks可以观察运行结果，无可视化
+# 功能：直接运行APP端签到，仿照MC的LOG写出LOGGER系统，可以观看运行结果
 
-import requests, time, re
+import requests, time, re, os, shutil, json
 import hashlib, random
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 
 FAKE_VERSION = '11.8.8.0'
 
@@ -24,6 +25,37 @@ APP_HEADERS = {
     'Accept-Encoding': 'gzip',
     'Cookie': 'ka=open',
 }
+
+def create_path(path: str):
+    os.path.exists(path) or os.mkdir(path)
+
+class Logger:
+    def __init__(self, prefix: str):
+        self.prefix = prefix
+        self.log_path = "log"
+        self.current_file = "latest.log" 
+        create_path(self.log_path)
+        if os.path.isfile(self.current_file):
+            new_name = '%s %s.log' % (self.prefix, time.strftime('%Y-%m-%d %H.%M.%S', time.localtime(os.path.getmtime(self.current_file))))
+            os.rename(self.current_file, new_name)
+            shutil.move(new_name, os.path.join(self.log_path, new_name))
+    
+    def log(self, message: str, mode: str = "INFO"):
+        with open(self.current_file, 'a+', encoding='utf-8') as file:
+            full_message = '[%s] [%s/%s]: %s\n' % (time.strftime('%H:%M:%S', time.localtime()), self.prefix, mode, message)
+            file.write(full_message)
+            print(full_message, end = '')
+    
+    def info(self, message: str):
+        self.log(message, "INFO")
+
+    def error(self, message: str):
+        self.log(message, "ERROR")
+
+    def warn(self, message: str):
+        self.log(message, "WARN")
+        
+LOGGER = Logger("TiebaSign")
 
 def random_string(length: int, table: 'str | list[str]' = '1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm') -> str:
     return ''.join(random.choice(table) for _ in range(length))
@@ -61,60 +93,114 @@ def sign_request_params(params: dict, is_fake: bool = True) -> dict:
 def to_url_params_string(params: dict) -> str:
     return '&'.join(f'{k}={v}' for k, v in params.items())
 
-cookies_text = ''
+with open('config.json', 'r', encoding = 'utf-8') as config_file:
+    cookies_texts = json.load(config_file)
 
-session = requests.session()
-session.headers = HEADERS
+i = 1
+for cookies_text in cookies_texts:
 
-for cookie in cookies_text.split('; '):
-    s = cookie.split('=', 1)
-    if s[0] == 'BDUSS':
-        BDUSS = s[1]
-    session.cookies.set(s[0], s[1])
+    LOGGER.info('导入第%d个COOKIE, 共%d个' % (i, len(cookies_texts)))
 
-def get_tbs() -> str:
+    error_tieba = []
+
+    session = requests.session()
     session.headers = HEADERS
-    return re.findall(r'PageData.tbs = "(.*?)";', session.get('https://tieba.baidu.com/').text)[0]
+    BDUSS = None
 
-tbs = get_tbs()
+    for cookie in cookies_text.split('; '):
+        s = cookie.split('=', 1)
+        if s[0] == 'BDUSS':
+            BDUSS = s[1]
+        session.cookies.set(s[0], s[1])
 
-def get_tieba_list(is_sign: bool = False) -> list[dict]:
-    session.headers = HEADERS
-    result = session.get('https://tieba.baidu.com/mo/q/newmoindex').json()
-    if result['error'] != 'success':
-        raise Exception("ERROR: %s" % result['error'])
-    return [{'kw': b['forum_name'], 'fid': b['forum_id']} for b in result['data']['like_forum'] if b['is_sign'] == is_sign and b['forum_id']]
+    if BDUSS == None or type(BDUSS) == type('') and len(BDUSS) == 0:
+        LOGGER.error('BDUSS未找到, 请检查当前COOKIE文本是否有误!')
+        continue
 
-def do_sign(params: dict) -> dict: #fid: str, kw: str
-    time.sleep(random.random() * 3 + 0.5)
-    params.update({
-        'BDUSS': BDUSS,
-        'tbs': tbs
+    def get_tbs() -> str:
+        session.headers = HEADERS
+        return re.findall(r'PageData.tbs = "(.*?)";', session.get('https://tieba.baidu.com/').text)[0]
+
+    try:
+        tbs = get_tbs()
+    except requests.TooManyRedirects:
+        LOGGER.error('重定向次数过多, TBS获取失败! 请检查COOKIE是否正确!')
+        continue
+    else:
+        LOGGER.info('TBS获取成功: ' + tbs)
+
+    def get_tieba_list(is_sign: bool = False) -> list[dict]:
+        session.headers = HEADERS
+        result = session.get('https://tieba.baidu.com/mo/q/newmoindex').json()
+        if result['error'] != 'success':
+            raise Exception("ERROR: %s" % result['error'])
+        return [{'kw': b['forum_name'], 'fid': b['forum_id']} for b in result['data']['like_forum'] if b['is_sign'] == is_sign and b['forum_id']]
+
+    def do_sign(params: dict) -> 'dict | None': #fid: str, kw: str
+        time.sleep(random.random() * 1.2 + 0.8)
+        params.update({
+            'BDUSS': BDUSS,
+            'tbs': tbs
+        })
+        session.headers = APP_HEADERS
+        result = session.post('http://c.tieba.baidu.com/c/c/forum/sign', data = to_url_params_string(sign_request_params(params)).encode('utf-8')).json()
+
+        if result['error_code'] == '340006':
+            LOGGER.warn('%s吧签到失败, 可能是这个吧出问题了' % (params['kw']))
+            
+            return None
+
+        return {
+            'name': params['kw'],
+            'bonus': result['user_info']['sign_bonus_point']
+        }
+
+    def do_sign_multiple(params: dict) -> 'dict | list[dict]': #forum_ids: list[str]
+        time.sleep(random.random() * 0.5 + 1)
+        params.update({
+            'BDUSS': BDUSS,
+            'tbs': tbs
+        })
+        session.headers = APP_HEADERS
+        result = session.post('http://c.tieba.baidu.com/c/c/forum/msign', data = to_url_params_string(sign_request_params(params))).json()
+        if result['error']['errno'] != '0':
+            return result['error']
+        else:
+            return result['info']
+
+    tieba_list = get_tieba_list()
+
+    if len(tieba_list) == 0:
+        LOGGER.info('该账号没有今天未签到的吧')
+        continue
+
+    LOGGER.info("贴吧列表获取完成, 共有%d个贴吧待签到, 列表:" % len(tieba_list))
+    LOGGER.info(", ".join(it['kw'] for it in tieba_list))
+
+    sign_multiple_result = do_sign_multiple({
+        'forum_ids': [it['fid'] for it in tieba_list][:199]
     })
-    session.headers = APP_HEADERS
-    result = session.post('http://c.tieba.baidu.com/c/c/forum/sign', data = to_url_params_string(sign_request_params(params)).encode('utf-8')).json()['user_info']
-    return {
-        'name': params['kw'],
-        'bonus': result['sign_bonus_point']
-    }
 
-def do_sign_multiple(params: dict) -> dict: #forum_ids: list[str]
-    time.sleep(random.random() * 0.5 + 0.8)
-    params.update({
-        'BDUSS': BDUSS,
-        'tbs': tbs
-    })
-    session.headers = APP_HEADERS
-    result = session.post('http://c.tieba.baidu.com/c/c/forum/msign', data = to_url_params_string(sign_request_params(params))).json()
-    if result['error']['errno'] != '0':
-        raise Exception("Sign failed")
-    return result['info']
+    if type(sign_multiple_result) == type({}):
+        LOGGER.error("批量签到失败, 错误信息: \n" + ';\n    '.join([f'{k}={v}' for k, v in sign_multiple_result.items()]))
+    else:
+        LOGGER.info("批量接口调用成功, 共签到成功%d个吧\n" % len(sign_multiple_result))
+        for result in sign_multiple_result:
+            LOGGER.info("%s吧: 连续签到%d天, +%d" % (result['forum_name'], result['sign_day_count'], result['cur_score']))
+    
+    tieba_list = get_tieba_list()
 
-do_sign_multiple({
-    'forum_ids': [it['fid'] for it in get_tieba_list()][:199]
-})
+    LOGGER.info("贴吧列表获取完成, 共有%d个贴吧待签到, 列表:" % len(tieba_list))
+    LOGGER.info(", ".join(it['kw'] for it in tieba_list))
 
-with ThreadPoolExecutor(max_workers = 3) as executor:
-    tasks = [executor.submit(do_sign, it) for it in get_tieba_list()]
+    with ThreadPoolExecutor(max_workers = 3) as executor:
+        tasks = [executor.submit(do_sign, it) for it in tieba_list]
+    wait(tasks, return_when = ALL_COMPLETED)
 
-pass
+    results = [t.result() for t in tasks]
+
+    LOGGER.info('签到结束, 单个签到共有%d个贴吧成功' % len([r for r in results if r != None]))
+    for result in results:
+        LOGGER.info("%s吧: +%d" % (result['name'], result['bonus']))
+
+    i += 1
